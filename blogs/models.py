b9268305b843ffb221c34bb741a29165352c4ff6
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-03 22:08:04 +0530

    Settingup models with meta configurations

--- blogs/models.py
+++ blogs/models.py
@@ -5,3 +5,10 @@ class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.category_name
