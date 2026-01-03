b9547b203beedee6b9e11fbb8e6fa9311dd00ee4
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-03 19:02:36 +0530

    Add template directory and create home page template

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to the Blog Home Page!");
+    return render(request, 'home.html')
