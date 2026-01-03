e0cf000f8a2ebf6f407911be384b41bf35443128
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-03 22:14:45 +0530

    MediaFiles Configuration

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -124,3 +124,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Mediafiles Configuration
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
