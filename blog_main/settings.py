f44820e609102103bcdcf5a00704e5e6b9f7f6d6
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-03 19:34:33 +0530

    Setting Up static FIles

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -114,4 +114,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "blog_main" / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
