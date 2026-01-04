df484b5462842a14b2640d379421996ec0238d14
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-04 18:41:48 +0530

    Populate Category Model to Frontend

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blogs.models import Category
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    # print(categories)
+    
+    return render(request, 'home.html', context)
