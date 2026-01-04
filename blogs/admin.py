156af1771729093bf8590b0ea40cc40aa83ba00b
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-04 17:45:44 +0530

    Populate Slug automatically

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Category, Blog
 
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'author', 'category', 'status', 'is_featured', 'created_at')
+    list_filter = ('status', 'is_featured', 'category', 'author')
+    search_fields = ('title', 'short_description', 'blog_body')
+    ordering = ('-created_at',)
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
