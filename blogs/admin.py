fb94a43381f923bbe64550a20df7076931b204e8
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-04 17:59:13 +0530

    Feature: Defining Serch Fields

--- blogs/admin.py
+++ blogs/admin.py
@@ -4,8 +4,8 @@ from .models import Category, Blog
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'author', 'category', 'status', 'is_featured', 'created_at')
+    search_fields = ('title', 'short_description', 'blog_body',"category__category_name", "author__username", "status")
     list_filter = ('status', 'is_featured', 'category', 'author')
-    search_fields = ('title', 'short_description', 'blog_body')
     ordering = ('-created_at',)
 
 # Register your models here.
