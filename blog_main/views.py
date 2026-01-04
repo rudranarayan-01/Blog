102dac5ddd48acd79d1bd4e3914a6e9c3ec4c733
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-04 19:19:33 +0530

    Rendering Published posts in frontend

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,10 +4,12 @@ from blogs.models import Category, Blog
 
 def home(request):
     categories = Category.objects.all()
-    featured_posts = Blog.objects.filter(is_featured=True).order_by('-created_at')[:5]
+    featured_posts = Blog.objects.filter(is_featured=True).order_by('-created_at')
+    posts = Blog.objects.filter(is_featured=False, status="Published").order_by('-created_at')
     context = {
         'categories': categories,
         'featured_posts': featured_posts,
+        'posts': posts,
     }
     
     # print(categories)
