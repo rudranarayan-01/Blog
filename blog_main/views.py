a3cbf7973dd9695b92b8d7d38ec90713fd8f8f5f
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-04 19:16:33 +0530

    Featured Posts in Frontend

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from blogs.models import Category
+from blogs.models import Category, Blog
 
 def home(request):
     categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True).order_by('-created_at')[:5]
     context = {
-        'categories': categories
+        'categories': categories,
+        'featured_posts': featured_posts,
     }
+    
     # print(categories)
     
     return render(request, 'home.html', context)
