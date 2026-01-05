3c6899e8357834a4ffe948c8405640f476d54ed5
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-05 12:45:50 +0530

    category URl Logic

--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
 def posts_by_category(request, category_id):
     # Logic to fetch posts by category_id
-    print(category_id)
-    return HttpResponse("Posts for category")
+    posts = Blog.objects.filter(status="Published", category=category_id)
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts_by_category.html', context)
