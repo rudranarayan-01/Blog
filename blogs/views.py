deca122611da4f18b15822015674e3529303cd99
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-05 12:37:28 +0530

    Setting Up Category Url

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def posts_by_category(request, category_id):
+    # Logic to fetch posts by category_id
+    print(category_id)
+    return HttpResponse("Posts for category")
