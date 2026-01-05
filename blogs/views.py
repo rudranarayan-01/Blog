ae38ad57aac2f9a8a0a3879df876157e956c50bf
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-05 13:06:53 +0530

    Dynmic Category routing

--- blogs/views.py
+++ blogs/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Blog
+from .models import Blog, Category
 
 # Create your views here.
 
 def posts_by_category(request, category_id):
     # Logic to fetch posts by category_id
     posts = Blog.objects.filter(status="Published", category=category_id)
+    category = Category.objects.get(pk=category_id)
     context = {
         'posts': posts,
+        'category': category,
     }
     return render(request, 'posts_by_category.html', context)
