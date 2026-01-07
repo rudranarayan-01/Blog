58db71333624ef22541bfdec1d0f5feb2a2ed25d
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-07 18:40:53 +0530

    Set as permission to add only only one about desc

--- background/admin.py
+++ background/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import AboutModel
 
+class AboutAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        count = AboutModel.objects.all().count()
+        if count == 0:
+            return True
+        return False
+    
 # Register your models here.
-admin.site.register(AboutModel)
+admin.site.register(AboutModel, AboutAdmin)
