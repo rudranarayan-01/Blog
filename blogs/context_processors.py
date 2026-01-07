545e2b9ea18142da6959bb6361c4858420839100
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-07 18:50:27 +0530

    Defining Context Processors for updating social Links

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,6 +1,11 @@
 from .models import Category
+from background.models import SocialLinks
 
 def get_categories(request):
     from blogs.models import Category
     categories = Category.objects.all()
     return {'categories': categories}
+
+def get_social_links(request):
+    social_links = SocialLinks.objects.first()
+    return {'social_links': social_links}
