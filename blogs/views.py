381f469eaddbfc80016e5e91fe4ac2baa11118be
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-07 22:18:23 +0530

    Search Article Functionality working

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Blog, Category
+from django.db.models import Q
 
 # Create your views here.
 
@@ -32,3 +33,16 @@ def blogs(request, slug):
         'single_blog': single_blog,
     }
     return render(request, 'blogs.html', context)
+
+def search_blogs(request):
+    keyword = request.GET.get('keyword', '')
+    if keyword:
+        blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status="Published")
+    else:
+        blogs = Blog.objects.none()  # Return an empty queryset if no keyword is provided
+
+    context = {
+        'blogs': blogs,
+        'keyword': keyword,
+    }
+    return render(request, 'search.html', context)
