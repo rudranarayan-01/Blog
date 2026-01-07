979456c0e396ac775a4dfcb53b3ac7665a21a90e
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-07 18:32:00 +0530

    About Page Updates

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,15 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from background.models import AboutModel
 from blogs.models import Category, Blog
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True).order_by('-created_at')
     posts = Blog.objects.filter(is_featured=False, status="Published").order_by('-created_at')
+    
+    # Fetch about section
+    try:
+        about = AboutModel.objects.get()
+    except AboutModel.DoesNotExist:
+        about = None
+
     context = {
         'featured_posts': featured_posts,
         'posts': posts,
+        'about': about,
     }
     
     # print(categories)
     
     return render(request, 'home.html', context)
+
