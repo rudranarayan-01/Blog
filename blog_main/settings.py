d8909442944d73f8552c1c3370ebbced587cdba2
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-21 22:39:26 +0530

    Designing Dashboard

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "background",
     "crispy_forms",
     "crispy_bootstrap4",
+    "dashboards",
 ]
 
 MIDDLEWARE = [
---
+++ dashboards/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Dashboard URLs will be defined here
+    path('', views.dashboard, name="dashboard"),
+]
