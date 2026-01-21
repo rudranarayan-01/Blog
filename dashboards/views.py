e9e2c898c93ef77283c3d949584b9e0083bfc57f
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-21 22:48:40 +0530

    Login routing to dashboard

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from blogs.models import Blog, Category
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required(login_url='login')
 def dashboard(request):
     category_count = Category.objects.all().count()
     blogs_count = Blog.objects.all().count()
