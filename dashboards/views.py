27745294753a425df88a9705af9974ba2f28c539
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-21 22:42:50 +0530

    Blog and category Count

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blogs.models import Blog, Category
 
 # Create your views here.
 
 def dashboard(request):
-    
-    return render(request, 'dashboards/dashboard.html')
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+
+    return render(request, 'dashboards/dashboard.html', {
+        'category_count': category_count,
+        'blogs_count': blogs_count
+    })
