271b742c19ce044213ec72570543a8eeeb27c17d
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-22 12:49:18 +0530

    Dashboard Designing

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # Dashboard URLs will be defined here
     path('', views.dashboard, name="dashboard"),
+    path('categories/', views.categories, name="categories"),
 ]
