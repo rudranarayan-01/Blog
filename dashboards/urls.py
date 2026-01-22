d75676c49a88d56b9c22292fe8e24aaa94b8fd8b
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-22 19:54:03 +0530

    Catagory CRUD in dashboard : Acomplished

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.dashboard, name="dashboard"),
     path('categories/', views.categories, name="categories"),
     path('categories/add/', views.add_category, name="add_category"),
-    path('categories/edit/<int:id>/', views.edit_category, name="edit_category"),
-    path('categories/delete/<int:id>/', views.delete_category, name="delete_category"),
+    path('categories/edit/<int:pk>/', views.edit_category, name="edit_category"),
+    path('categories/delete/<int:pk>/', views.delete_category, name="delete_category"),
     # path('blogs/', views.blogs, name="blogs"),
 ]
