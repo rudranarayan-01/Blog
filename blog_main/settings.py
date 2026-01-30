8c8fbd44fc8728aaaa58a43ccf80a15c11c07574
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-30 19:29:17 +0530

    Update settings.py

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-4n+79r!xx5s$+dwzx5gfb!e&3iye_ttjnon9=ac7+97!2=(5uh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://techie148.pythonanywhere.com"]
 
 
 # Application definition
